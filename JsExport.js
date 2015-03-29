0fc8e6df53657b85ea5f744187f5d384a2685ecb
ravichaitanya mukkavilli <ravichaitanyamukkavilli@users.noreply.github.com>
2015-03-29 22:28:26 +0530

    Update JsExport.js

--- JsExport.js
+++ JsExport.js
@@ -1,29 +1,72 @@
-(function ($) {
+        (function ($) {
 
-    $.fn.jsExport = function (options) {
+            $.fn.jsExport = function (options) {
 
-        // This is the easiest way to have default options.
-        var settings = $.extend({
-            // These are the defaults.
-            type: "excel",
-            data: ""
-        }, options);
+                // This is the easiest way to have default options.
+                var settings = $.extend({
+                    // These are the defaults.
+                    type: "excel",
+                    data: "",
+                    paging: false
+                }, options);
 
-        switch (settings.type) {
+                switch (settings.type) {
 
-            case "excel":
-                window.open("data:application/vnd.ms-excel," + encodeURIComponent(settings.data));
-                break;
-            case "doc":
-                window.open("data:application/msword," + encodeURIComponent(settings.data));
-                break;
-            case "ppt":
-                window.open("data:application/vnd.ms-powerpoint," + encodeURIComponent(settings.data));
-                break;
-            case "xml":
-                window.open("data:application/xml," + encodeURIComponent(settings.data));
-                break;
-        }
-        return true;
+                    case "excel":
+                        if (settings.paging) {
+                            var table = makeTable(settings.data);
+                            
+                            window.open("data:application/vnd.ms-excel," + encodeURIComponent(table));
+                        }
+                        else {
+                            window.open("data:application/vnd.ms-excel," + encodeURIComponent(settings.data));
+                        }
+                        
+                        break;
+                    case "doc":
+                        if (settings.paging) {
+                            var table = makeTable(settings.data);
+                            window.open("data:application/msword," + encodeURIComponent(table));
+                        }
+                        else {
+                            window.open("data:application/msword," + encodeURIComponent(settings.data));
+                        }
+                        
+                        break;
+                  
+                    case "xml":
+                        if (settings.paging) {
+                            var table = makeTable(settings.data);
+                            window.open("data:application/xml," + encodeURIComponent(table));
+                        }
+                        else {
+                            window.open("data:application/xml," + encodeURIComponent(settings.data));
+                        }
+                        
+                        break;
+                    
+                }
+                function makeTable(data) {
+                    var table = '<div id=tempdiv><table id="tempid">';
+                    var tbl_body = "";
+
+                    var HeadKeys = "";
+
+                    $.each($.parseJSON(data), function () {
+                        var tbl_row = "";
+                        HeadKeys = '';
+                        $.each(this, function (k, v) {
+                            tbl_row += "<td>" + v + "</td>";
+                            HeadKeys += '<td><b>' + k + '</b></td>';
+                        })
+                        tbl_body += "<tr>" + tbl_row + "</tr>";
+                    });
+                    table += HeadKeys + "</tr>" + tbl_body + '</table></div>';
+                    return table;
+                }
+                
+                return true;
+            };
+        }(jQuery));
     };
 }(jQuery));
