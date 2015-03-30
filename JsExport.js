d19891e9efe0f42cc5d5eaf9b8210972bfc502b8
ravichaitanya mukkavilli <ravichaitanyamukkavilli@users.noreply.github.com>
2015-03-30 11:10:06 +0530

    Update JsExport.js

--- JsExport.js
+++ JsExport.js
@@ -1,20 +1,22 @@
-        (function ($) {
+     (function ($) {
 
-            $.fn.jsExport = function (options) {
+            $.jsExport = function (options) {
 
                 // This is the easiest way to have default options.
                 var settings = $.extend({
                     // These are the defaults.
                     type: "excel",
                     data: "",
-                    paging: false
+                    paging: false,
+                    alternateRowColor: "white",
+                    HeaderColor:"white"
                 }, options);
 
                 switch (settings.type) {
 
                     case "excel":
                         if (settings.paging) {
-                            var table = makeTable(settings.data);
+                            var table = makeTable(settings.data.d);
                             
                             window.open("data:application/vnd.ms-excel," + encodeURIComponent(table));
                         }
@@ -35,31 +37,31 @@
                         break;
                   
                     case "xml":
-                        if (settings.paging) {
-                            var table = makeTable(settings.data);
-                            window.open("data:application/xml," + encodeURIComponent(table));
-                        }
-                        else {
-                            window.open("data:application/xml," + encodeURIComponent(settings.data));
-                        }
-                        
+                        window.open("data:application/xml," + encodeURIComponent(settings.data));
                         break;
                     
                 }
                 function makeTable(data) {
-                    var table = '<div id=tempdiv><table id="tempid">';
+                    var table = '<div id=tempdiv><table id="tempid" border="1" ;style="width:100%; height:100%" >';
                     var tbl_body = "";
 
                     var HeadKeys = "";
-
+                    var i = 0;
                     $.each($.parseJSON(data), function () {
                         var tbl_row = "";
                         HeadKeys = '';
                         $.each(this, function (k, v) {
-                            tbl_row += "<td>" + v + "</td>";
-                            HeadKeys += '<td><b>' + k + '</b></td>';
+                          
+                            tbl_row += "<td style='text-align:center'>" + v + "</td>";
+                            HeadKeys += "<td style=background-color:'" + settings.HeaderColor + "';text-align:center><b>" + k + '</b></td>';
                         })
-                        tbl_body += "<tr>" + tbl_row + "</tr>";
+                        if (i%2==0) {
+                            tbl_body += "<tr style=background-color:'" + settings.alternateRowColor + "';text-align:center>" + tbl_row + "</tr>";
+                        }
+                        else {
+                            tbl_body += "<tr style=text-align:center>" + tbl_row + "</tr>";
+                        }
+                        i++;
                     });
                     table += HeadKeys + "</tr>" + tbl_body + '</table></div>';
                     return table;
@@ -68,5 +70,3 @@
                 return true;
             };
         }(jQuery));
-    };
-}(jQuery));
